e4f02fecd093a8e198f5f1a0d7ba2bdd14f6359b
oldskoolfan <harris.1305@gmail.com>
2018-11-21 11:58:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php require "{$_SERVER['DOCUMENT_ROOT']}/include/nav.php" ?>
 <section class="home">
 	<p class="opening">
-	In 1999 Nature’s Nanny was started by me, Christine Burkitt, “The Nanny” at the urging of my husband Scott (Nature’s Daddy) to help fulfill my passion for loving and caring for others pets as I would want my own cared for in my absence. I, being the proud parent of multiple pets all of my life, know first hand the concern and anxiety that goes along with selecting loving care for our pets when we need to be away.  And so, Nature’s Nanny in-home dog and pet sitting came to be.  It has been our pleasure for over 13 years to care for a diverse group of furry and feathered guests in our home. Scott and I currently are the proud parents of 3 rescue dogs, the gift of a golden retriever and a rescue macaw. While this is enough for most, our motto is… “there is always room for one more…”
+	In 1999 Nature’s Nanny was started by me, Christine Burkitt, “The Nanny” at the urging of my husband Scott (Nature’s Daddy) to help fulfill my passion for loving and caring for others pets as I would want my own cared for in my absence. I, being the proud parent of multiple pets all of my life, know first hand the concern and anxiety that goes along with selecting loving care for our pets when we need to be away.  And so, Nature’s Nanny in-home dog and pet sitting came to be.  It has been our pleasure for over 19 years to care for a diverse group of furry and feathered guests in our home. Scott and I currently are the proud parents of 3 rescue dogs, the gift of a golden retriever and a rescue macaw. While this is enough for most, our motto is… “there is always room for one more…”
 	</p>
 	<section>
 		<div class="img-link">
